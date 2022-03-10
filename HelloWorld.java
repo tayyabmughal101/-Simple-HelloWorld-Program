8161a56981d769895edcd4c50f3fdec75653ca34
Tayyab Mughal <89740522+tayyabmughal101@users.noreply.github.com>
2022-03-10 19:27:45 +0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,17 @@
-class HelloWorld {
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author Tayyab
+ */
+public class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello World!");
     }
 }
